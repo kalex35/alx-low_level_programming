fac00ef2bf048548035feb4b20940b1f755ebb59
Alexander <greatkali35@gmail.com>
2023-03-16 18:08:05 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints without the function printf or puts
+ * Return: 1 (success)
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
