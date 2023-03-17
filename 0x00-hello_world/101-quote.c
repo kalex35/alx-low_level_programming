76e54cbfc6228306dfcc70ce4b03b286dcea355c
Alexander <greatkali35@gmail.com>
2023-03-16 18:23:10 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A program that prints without the function printf or puts
- * Return: 1 (success)
-*/
+ * main - Program entry point
+ * Description: Prints the specified string
+ * Return: 1
+ */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
