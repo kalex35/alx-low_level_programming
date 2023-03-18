e3bd932f0f058906a5f37d2e6ca55f6288e5db46
Alexander <greatkali35@gmail.com>
2023-03-18 05:35:22 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,8 +5,8 @@
 
 
 /**
- * main - Write a program that prints all possible different combinations
- * of three digits.
+ * main - Write a program that prints all possible combinations
+ * of two two-digit numbers.
  * Return: Always 0 (success)
 */
 
