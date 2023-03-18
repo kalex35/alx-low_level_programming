8092b905aa793033cbaac38c70a7ce9f7c8dab10
Alexander <greatkali35@gmail.com>
2023-03-18 04:30:28 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
 
 /**
- * main - Prints numbers between 0 to 9 and letters between a to f
- *
+ * main - Write a program that prints all the numbers of base 16 in
+ * lowercase, followed by a new line.
  * Return: Always 0 (success)
 */
+
 int main(void)
+
 {
 	int d;
 	char low;
 
-	for (d = 0; d <= '9'; d++)
+	for (d = '0'; d <= '9'; d++)
 	putchar(d);
 
 	for (low = 'a'; low <= 'f'; low++)
